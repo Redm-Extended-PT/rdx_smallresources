1eb61f4a5ba9eee4305a22aabc2ed272839c7063
Nevermind <58900773+RalivTV@users.noreply.github.com>
2022-10-02 17:31:37 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- Config WeaponDmg/MeleeDmg 1.0 is default, I find 3.4 is perfect but you change change it however you like. Weapon DMG is the dmg on gun shots and melee is fist dmg.
+Config.WeaponDmg = 3.40
+Config.MeleeDmg = 0.65
+
+-- Config Traffic
+Config.Traffic = {
+    pedFrequency = 5.3,
+    trafficFrequency = 0.0,
+    animalFrequency = 5.3,
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+game 'rdr3'
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'Nevermind/Vision'
+description 'rdx_smallresources'
+version '1.0'
+
+client_script {
+    'client.lua',
+	'config.lua'
+}
